4bac8dd289c24fb529d79ce16dd5c011a1f8681d
meiezbr <mayezbr@outlook.fr>
2024-09-27 23:28:03 +0100

    tweaking

--- js/main.js
+++ js/main.js
@@ -317,3 +317,20 @@ $(function() {
 
 });
 
+// Show the button when the user scrolls down 20px from the top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  const scrollBtn = document.getElementById("scrollBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+
+// Scroll to the top when the button is clicked
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
